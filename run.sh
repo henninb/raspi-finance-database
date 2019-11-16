e20b4f7c74d77a97656ac50caad090b597c16b98
Brian Henning <henninb@msn.com>
2019-11-16 10:11:55 -0600

    updates

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 cat aaaa_tables.sql zzzz_tables.sql > master.sql
 
 #psql -h $(ipconfig getifaddr en0) -p 5432 -d postgres -U henninb < master.sql > log.txt 2>&1
-psql -h $(hostname -I | awk '{print $1}') -p 5432 -d postgres -U henninb < master.sql > log.txt 2>&1
+psql -h $(hostname -i | awk '{print $1}') -p 5432 -d postgres -U henninb < master.sql > log.txt 2>&1
 
 #echo psql finance_db -U henninb -h $(ipconfig getifaddr en0)
-echo psql finance_db -U henninb -h $(hostname -I | awk '{print $1}')
+echo psql finance_db -U henninb -h $(hostname -i | awk '{print $1}')
 
 exit 0
