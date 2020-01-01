b3ea0c1034347b7f437d0c28d0a998037d73748d
Brian Henning <henninb@msn.com>
2020-01-01 09:37:59 -0600

    updates for run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,14 @@
 
 cat aaaa_tables.sql zzzz_tables.sql > master.sql
 
-#psql -h $(ipconfig getifaddr en0) -p 5432 -d postgres -U henninb < master.sql > log.txt 2>&1
-psql -h $(hostname -i | awk '{print $1}') -p 5432 -d postgres -U henninb < master.sql > log.txt 2>&1
+if [ "$OS" = "Darwin" ]; then
+  HOST_IP=$(ipconfig getifaddr en0)
+else
+  HOST_IP=$(hostname -i | awk '{print $1}')
+fi
 
-#echo psql finance_db -U henninb -h $(ipconfig getifaddr en0)
-echo psql finance_db -U henninb -h $(hostname -i | awk '{print $1}')
+psql -h ${HOST_IP} -p 5432 -d postgres -U henninb < master.sql > log.txt 2>&1
+
+echo psql finance_db -U henninb -h ${HOST_IP}
 
 exit 0
