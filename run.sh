dcd8034b8faee857b2d8dd4372dad2deca44ddcc
Brian Henning <henninb@msn.com>
2020-01-05 19:36:30 -0600

    run update

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ else
   HOST_IP=$(hostname -i | awk '{print $1}')
 fi
 
+echo postgresql database password
 psql -h ${HOST_IP} -p 5432 -d postgres -U henninb < master.sql > log.txt 2>&1
 
 echo psql finance_db -U henninb -h ${HOST_IP}
