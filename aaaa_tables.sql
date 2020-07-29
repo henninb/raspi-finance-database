d57348151b1b334fc8358c26a612754d2255bcc7
Brian Henning <henninb@msn.com>
2020-07-29 06:43:51 -0500

    cleanup database code

--- aaaa_tables.sql
+++ aaaa_tables.sql
@@ -20,10 +20,6 @@ REVOKE CONNECT ON DATABASE finance_db FROM public;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 SET client_min_messages TO WARNING;
 
---create the SEQUENCE prior to the table.
---DROP SEQUENCE IF EXISTS t_account_account_id_seq CASCADE;
---to_timestamp(0)
-
 CREATE SEQUENCE t_payment_payment_id_seq START WITH 1001;
 
 --TRUNCATE TABLE IF EXISTS t_payment;
@@ -56,11 +52,6 @@ CREATE TABLE IF NOT EXISTS t_account(
 );
 ALTER TABLE t_account ADD PRIMARY KEY (account_id);
 
---ALTER TABLE t_account ADD PRIMARY KEY (account_id);
---ALTER TABLE t_account ALTER COLUMN account_id set DEFAULT nextval('t_account_account_id_seq');
---ALTER TABLE t_account ADD DEFAULT nextval('t_account_account_id_seq') (account_id);
-
---create unique index account_id_idx on t_account(account_id);
 CREATE UNIQUE INDEX account_name_owner_idx on t_account(account_name_owner);
 
 CREATE OR REPLACE FUNCTION fn_upd_ts_account() RETURNS TRIGGER AS
@@ -88,8 +79,6 @@ $$ LANGUAGE PLPGSQL;
 DROP TRIGGER IF EXISTS tr_ins_ts_account on t_account;
 CREATE TRIGGER tr_ins_ts_account BEFORE INSERT ON t_account FOR EACH ROW EXECUTE PROCEDURE fn_ins_ts_account();
 
---create the SEQUENCE prior to the table.
---DROP SEQUENCE IF EXISTS t_summary_summary_id_seq CASCADE;
 CREATE SEQUENCE t_summary_summary_id_seq start with 1001;
 
 --TRUNCATE TABLE IF EXISTS t_summary;
@@ -120,15 +109,6 @@ CREATE TABLE IF NOT EXISTS t_transaction_categories(
   transaction_id BIGINT NOT NULL
 );
 
---Actually nextval will advance sequence and return the new value
---SELECT NEXTVAL('t_summary_summary_id_seq');
-
--- ************************************** --
--- *** t_transaction table operations *** --
--- ************************************** --
-
---create the SEQUENCE prior to the table.
---DROP SEQUENCE IF EXISTS t_transaction_transaction_id_seq CASCADE;
 CREATE SEQUENCE t_transaction_transaction_id_seq start with 1001;
 
 --TRUNCATE TABLE IF EXISTS t_transaction;
@@ -149,8 +129,6 @@ CREATE TABLE IF NOT EXISTS t_transaction (
   notes TEXT DEFAULT '',
   date_updated TIMESTAMP DEFAULT TO_TIMESTAMP(0),
   date_added TIMESTAMP DEFAULT TO_TIMESTAMP(0)
-  --CONSTRAINT t_transaction_pk PRIMARY KEY (guid)
-  --CONSTRAINT t_transaction_unique UNIQUE (guid)
 );
 ALTER TABLE t_transaction ADD PRIMARY KEY (transaction_id);
 
@@ -185,31 +163,7 @@ $$ LANGUAGE PLPGSQL;
 
 DROP TRIGGER IF EXISTS tr_upd_ts_transactions on t_transaction;
 CREATE TRIGGER tr_upd_ts_transactions BEFORE UPDATE ON t_transaction FOR EACH ROW EXECUTE PROCEDURE fn_upd_ts_transaction();
-
---SELECT * FROM information_schema.constraint_table_usage WHERE table_name = 't_transaction';
---ALTER TABLE t_transaction DROP CONSTRAINT guid_idx;
---ALTER TABLE sample.public.employee DROP CONSTRAINT test_const
---DROP INDEX guid_idx;
---DROP TRIGGER tr_upd_ts_account  on t_account;
---DROP TRIGGER tr_upd_ts_transactions  on t_transaction;
---DROP TABLE IF EXISTS t_transaction_reoccur;
-
---CREATE TABLE IF NOT EXISTS t_transaction_reoccur (
---  account_id BIGINT,
---  account_type VARCHAR(6),
---  account_name_owner VARCHAR(40) NOT NULL,
---  guid VARCHAR(36) NOT NULL,
---  sha256 VARCHAR(70),
---  transaction_date TIMESTAMP NOT NULL,
---  description VARCHAR(75) NOT NULL,
---  category VARCHAR(50),
---  amount DECIMAL(12,2) NOT NULL,
---  cleared INTEGER,
---  notes VARCHAR(100)
---);
-
-select conrelid::regclass AS table_from, conname, pg_get_constraintdef(c.oid) from pg_constraint c join pg_namespace n ON n.oid = c.connamespace where  contype in ('f', 'p','c','u') order by contype;
-
+SELECT conrelid::regclass AS table_from, conname, pg_get_constraintdef(c.oid) FROM pg_constraint c JOIN pg_namespace n ON n.oid = c.connamespace WHERE contype in ('f', 'p','c','u') ORDER BY contype;
 
 INSERT INTO t_category(category) VALUES('adjustment');
 INSERT INTO t_category(category) VALUES('allina');
