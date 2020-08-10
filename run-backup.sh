a439cc93519380b04816732cc05b80b8016c2b92
Brian Henning <henninb@msn.com>
2020-08-10 17:51:18 -0500

    backup updates

--- run-backup.sh
+++ run-backup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+date=$(date '+%Y-%m-%d')
+
 if [ "$OS" = "Darwin" ]; then
   HOST_IP=$(ipconfig getifaddr en0)
 else
@@ -7,6 +9,6 @@ else
 fi
 
 echo postgresql database password
-pg_dump -h "${HOST_IP}" -p 5432 -U henninb -W -F t -d finance_db > finance_db.tar
+pg_dump -h "${HOST_IP}" -p 5432 -U henninb -W -F t -d finance_db > finance_db-v12-3-${date}.tar
 
 exit 0
