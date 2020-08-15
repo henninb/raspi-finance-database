f550bc7e7c0f02f92d68276796959223dbf7ebf8
Brian Henning <henninb@msn.com>
2020-08-15 06:16:22 -0500

    updates to restore

--- run-restore.sh
+++ run-restore.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <restore-tar-file>"
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+  echo "Usage: $0 <restore-tar-file> [port]"
   exit 1
 fi
 
 date=$(date '+%Y-%m-%d')
 restore_filename=$1
 port=5432
+
+if [ -n "$2" ]; then
+  port=$2
+fi
+echo "port is set to '$port'."
 
 if [ "$OS" = "Darwin" ]; then
   HOST_IP=$(ipconfig getifaddr en0)
@@ -16,9 +21,9 @@ else
 fi
 
 echo postgresql database password
-if psql -h "${HOST_IP}" -p ${port} -d postgres -U henninb < finance_db-drop.sql; then
+if psql -h "${HOST_IP}" -p "${port}" -d postgres -U henninb < finance_db-drop.sql; then
   echo postgresql database password
-  pg_restore -h "${HOST_IP}" -p ${port} -U henninb -F t -d finance_db --verbose "${restore_filename}" | tee -a "finance-db-restore-${date}.log"
+  pg_restore -h "${HOST_IP}" -p "${port}" -U henninb -F t -d finance_db --verbose "${restore_filename}" | tee -a "finance-db-restore-${date}.log"
 else
   echo "failed to drop the old database [finance_db]."
 fi
