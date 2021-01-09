01c41439e8c9546c2fc4978218ae0e13b4d483e6
Brian Henning <henninb@msn.com>
2021-01-09 17:52:42 -0600

    schema updates

--- run-backup.sh
+++ run-backup.sh
@@ -71,7 +71,7 @@ psql -h "${server}" -p "${port}" -U ${username} finance_db -c "\copy (SELECT par
 psql -h localhost -p "${port}" -U "${username}" finance_fresh_db -c "\copy t_parm FROM 't_parm.csv' CSV HEADER; commit"
 
 echo transaction
-psql -h "${server}" -p "${port}" -U ${username} finance_db -c "\copy (SELECT transaction_id, account_id, account_type, account_name_owner, guid, transaction_date, description, category, amount, transaction_state, reoccurring, reoccurring_type, active_status, notes, receipt_image_id, date_updated, date_added from t_transaction ORDER BY transaction_id) TO 't_transaction.csv' CSV HEADER"
+psql -h "${server}" -p "${port}" -U ${username} finance_db -c "\copy (SELECT transaction_id, account_id, account_type, account_name_owner, guid, transaction_date, due_date, description, category, amount, transaction_state, reoccurring, reoccurring_type, active_status, notes, receipt_image_id, date_updated, date_added from t_transaction ORDER BY transaction_id) TO 't_transaction.csv' CSV HEADER"
 psql -h localhost -p "${port}" -U "${username}" finance_fresh_db -c "\copy t_transaction FROM 't_transaction.csv' CSV HEADER; commit"
 
 echo transaction_categories
@@ -83,7 +83,7 @@ psql -h "${server}" -p "${port}" -U ${username} finance_db -c "\copy (SELECT pay
 psql -h localhost -p "${port}" -U "${username}" finance_fresh_db -c "\copy t_payment FROM 't_payment.csv' CSV HEADER; commit"
 
 echo receipt_image
-psql -h "${server}" -p "${port}" -U ${username} finance_db -c "\copy (SELECT receipt_image_id, transaction_id, jpg_image, active_status, date_updated, date_added from t_receipt_image ORDER BY receipt_image_id) TO 't_receipt_image.csv' CSV HEADER"
+psql -h "${server}" -p "${port}" -U ${username} finance_db -c "\copy (SELECT receipt_image_id, transaction_id, image, thumbnail, image_format_type, active_status, date_updated, date_added from t_receipt_image ORDER BY receipt_image_id) TO 't_receipt_image.csv' CSV HEADER"
 
 psql -h localhost -p "${port}" -U "${username}" finance_fresh_db -c "\copy t_receipt_image FROM 't_receipt_image.csv' CSV HEADER; commit"
 
