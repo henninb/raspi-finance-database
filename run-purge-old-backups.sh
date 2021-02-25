fa9ca2a298de39855d8f8f06c7b94605a51f3dfb
Brian Henning <henninb@msn.com>
2021-02-25 17:15:46 -0600

    updates

--- run-purge-old-backups.sh
+++ run-purge-old-backups.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # find backups older than 90 days
-find . -maxdepth 1 -type f -mtime +90 -name "finance_db*.tar" -print0
+find . -maxdepth 1 -type f -mtime +90 -name "finance_db*.tar"
 
 find . -maxdepth 1 -type f -mtime +90 -name "finance_db*.tar" -print0 | xargs -0 rm
 
