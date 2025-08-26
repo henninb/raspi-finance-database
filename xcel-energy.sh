542511bca6fd41705923c8a97f6b0351bd7e2478
Brian Henning <henninb@msn.com>
2025-08-26 06:49:10 -0500

    wip

--- xcel-energy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#psql -h finance-db -U henninb -d finance_db -c "\COPY t_transaction FROM 'xcel-energy.csv' WITH CSV HEADER"
-psql -h finance-db -U henninb -d finance_db -f xcel-energy.sql
-
-exit 0
